32652680e5a22112b6eedeb58857e7eae7e54c90
ilkkantuluce07 <60572381+ilkkantuluce07@users.noreply.github.com>
2021-10-17 01:13:53 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ function myFunction() {
     const fullName = name.split(" ");
 
     var URL = 'https://careers.ulta.com/careers/jobs?categories=Distribution%20Center%20Associates%7CDistribution%20Center%20Management&page=1&locations=' + pickDc + '&iis=DC_Referral&iisn=' + email + ',' + id + ',' + fullName;
-  
+    
     if(name.length == 0 || email.length == 0 || id.length == 0) {
         var link = document.getElementById('link');
         link.href = 'javascript:;'
@@ -60,34 +60,31 @@ function copyLink() {
     
 }
 
-function shareData() {
-    let shareData = {
-        title: 'MDN',
-        text: 'Learn web development on MDN!',
-        url: 'https://developer.mozilla.org',
-    }
-
-    navigator.share(shareData)
-        .then(() =>
-        alert('MDN shared successfully')
-        )
-        .catch((e) =>
-        alert('Error: ' + e)
-        )
-}
-
 
 const shareBtn = document.querySelector('#shareBtn');
 shareBtn.onclick = async function() {
-  if(navigator.share) {
-    const shareData = {
-      title: 'Web Share Demo',
-      text: 'This is some text I wanted to share',
-      url: 'https://josephkhan.me'
-    };
-    await navigator.share(shareData);
-    console.log('Share Successfull');
-  } else {
-    alert('Web Share not supported');
-  }
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var id = document.getElementById("id").value;
+    var pickDc = document.getElementById("pick-dc").value;
+
+    var shareText = document.getElementById("copyLink").value;
+
+    if(name.length == 0 || email.length == 0 || id.length == 0) {
+        alert('Fill all inputs')
+    }
+    else {
+        if(navigator.share) {
+            const shareData = {
+              url: shareText
+            };
+                await navigator.share(shareData);
+                console.log('Share Successfull');
+            } else {
+                alert('Web Share not supported');
+          }
+    }
+
+  
 }
